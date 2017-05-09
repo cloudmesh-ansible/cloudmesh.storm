ba27c9b5dc35853855ae7ba19add95f1571c9a35
Ajit Balaga <argetlam115@github.com>
2017-05-09 11:48:32 -0400

    new:usr: Added readme.md and changed a few ansible files

--- cloudmesh/storm/command/storm.py
+++ cloudmesh/storm/command/storm.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
-
+from cloudmesh.common.default import Default
+from cloudmesh.common.console import Console
 
 class StormCommand(PluginCommand):
 
@@ -12,6 +13,10 @@ class StormCommand(PluginCommand):
 
           Usage:
                 storm deploy CLOUD
+                storm info
+                storm status
+                storm cloud
+                storm image
                 storm -f FILE
                 storm FILE
                 storm list
@@ -25,7 +30,16 @@ class StormCommand(PluginCommand):
               -f      specify the file
 
         """
-        print(arguments)
-
-
-
+        
+        default = Default()
+        cloud = arguments.CLOUD or default["global"]["cloud"]
+        
+        arguments.user = arguments["--user"]
+        
+        if arguments.info:
+        	Console.error("Not yet implemented")
+        elif arguments.cloud:
+        	default["storm","cloud"] = arguments.cloud
+        elif arguments.image:
+        	default["storm","image"] = arguments.image
+        
