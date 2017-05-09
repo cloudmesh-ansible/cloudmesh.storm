cad84fec4274b7e1a313fb5921250f46229eeab0
Gregor von Laszewski <laszewski@gmail.com>
2017-05-09 06:36:35 -0400

    new:usr: add cmd5 command

---
+++ cloudmesh/storm/command/storm.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from cloudmesh.shell.command import command
+from cloudmesh.shell.command import PluginCommand
+
+
+class StormCommand(PluginCommand):
+
+    @command
+    def do_storm(self, args, arguments):
+        """
+        ::
+
+          Usage:
+                storm -f FILE
+                storm FILE
+                storm list
+
+          This command does some useful things.
+
+          Arguments:
+              FILE   a file name
+
+          Options:
+              -f      specify the file
+
+        """
+        print(arguments)
+
+
+
